00f62cebf23fb574e2c49834dec03fb7f8bd30d2
Saferill <saferill@example.com>
2026-05-07 22:43:17 +0700

    cleanup: remove legacy temp_donghua files to prevent build conflicts

--- temp_donghua/donghuaweb-main/src/utils/convertIdToTitle.ts
+++
@@ -1,8 +0,0 @@
-export default function convertIdToTitle(str: string): string {
-  return str
-    .split("-")
-    .map((item) => {
-      return item[0].toUpperCase() + item.slice(1);
-    })
-    .join(" ");
-}
--- temp_donghua/donghuaweb-main/src/utils/generateUrlPath.ts
+++
@@ -1,7 +0,0 @@
-import path from "path";
-
-export default function generateUrlPath(...paths: string[]): string {
-  const joinedPath = path.join(...paths);
-
-  return joinedPath.replace(/\\/g, "/");
-}
